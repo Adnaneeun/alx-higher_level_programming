27d951693fe7ee609dae4501a23e832a682681c3
Adnan <adnan.b.hasan@gmail.com>
2023-06-06 18:16:38 -0700

    python if, else, loops functions

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,22 +1,7 @@
 #!/usr/bin/python3
-for i in range(122, 96, -1):
-    if i % 2 != 0:
-        i = i - 32
-    print("{}".format(chr(i)), end="")
+# Author - Godswill Kalu
 
-
-
-
-
-vi 101-remove_char_at.py
-
-#!/usr/bin/python3
-def remove_char_at(str, n):
-    new = ""
-    i = 0
-    for c in str:
-        if i != n:
-            new += c
-        i += 1
-    return new
-
+i = 0
+for c in range(ord('z'), ord('a') - 1, -1):
+    print("{}".format(chr(c - i)), end="")
+    i = 32 if i == 0 else 0
